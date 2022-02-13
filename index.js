a2b3aea7fe5f7af7ff48c5175e7c55ba1ed24b03
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-02-13 15:28:41 -0600

    questions, answers, and choices are working in the console

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
 
-inquirer.prompt([
+// TODO: Create an array of questions for user input
+const questions = [
     {
         name: 'title',
-        message: 'Enter your repository title here',
-        type: 'imput'
-    }])
-    .then(function(answer) {
-        console.log(answer);
-    })
+        message: 'Enter title of repository here: ',
+        type: 'input'
+    },
+    {
+        name: 'description',
+        message: 'Describe application in 1-2 sentences: ',
+        type: 'input'
+    },
+    {
+        name: 'installation',
+        message: 'List step-by-step instructions on how to install your project',
+        type: 'input'
+    },
+    {
+        name: 'screenshots',
+        message: 'Copy and paste the relative path of your photo inside of parentheses',
+        type: 'input'
+    },
+    {
+        name: 'license',
+        message: 'Which license will you use for your project?',
+        type: 'list',
+        choices: ['MIT', 'IBM', 'MOZILLA'],
+    },
+];
+
+inquirer.prompt(questions).then(answer => {
+    console.log(answer);
+    
+});
+
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(data) {
+    
+}
+
+
+// TODO: Create a function to initialize app
+function init() {
+
+}
+
+// TODO: Function call to initialize app
+init();
---
+++ sample.md
@@ -0,0 +1,44 @@
+
+  # heat wave
+
+  ---
+
+  ## Description 
+
+  alsdkfjsldf
+
+  ---
+
+  ## Table of Contents:
+
+  *[Installation](#installation)
+
+  *[Usage](#usage)
+
+  *[License](#license)
+
+  *[Questions](#questions)
+
+  ---
+
+  ### Installation
+
+  Install the necessary dependencies below:
+
+  hellloooooo giursasdfas
+
+  ---
+
+  ### screenshots
+
+  
+
+  ![Image 1](utils/assets/images/heatwaveSS1.png)
+
+  ---
+
+  ### License
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
